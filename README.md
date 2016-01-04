04e491baeeb8ad0e5a1abff698b3d57cf2351a43
Kevin Moore <jauntbox@gmail.com>
2016-01-03 17:25:19 -0800

    Updated code with easier ways to specify trip endpoints and data dictionary generation. Updated usage instructions.

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-Track and plot Uber surge multipliers as a function of time
+SurgeTrends is a pyton script to collect and plot Uber surge multipliers as a function of time.
 
 Instructions:
-* Register for an API key with Uber (https://developer.uber.com/)
-* Create a file called "config.py" with a single line specifying your server token: server_token='string you got from Uber'
-* Run the code via "python surge.py"
-* Look through the iPython notebook for crude examples on how to plot the results
+* Register for an API key with Uber: [https://developer.uber.com/](https://developer.uber.com/)
+* Create a file called `config.py` with a single line specifying your server token: `server_token="string you got when registering with Uber's API"`
+* Define a set of location endpoints you'd like you use for your trips in the locations dictionary in the `surge.py` file
+* There is example code for either hard-coding trips between selected endpoints or generating all possible trips between your defined endpoints. Note that Uber limits you to 1000 API calls per hour so keep this in mind when specifying your trips and time between queries.
+* Choose an output file to store your data in (look for the `output_file` variable)
+* Run the code via `python surge.py`
+* Look through the iPython notebook file `make_plots.ipynb` for examples on how to plot the results. Hopefully it's documented well enough to follow.
--- surge.py
+++ surge.py
@@ -15,135 +15,66 @@ uber_types = ['uberX', 'uberXL', 'uberSELECT', 'UberBLACK', 'UberSUV']
 #num_trips = 4
 #trips = [None]*num_trips
 
+locations = {}
+locations['DNA'] = {
+	'latitude': 37.7589,
+	'longitude': -122.4125
+}
+locations['Marina'] = {
+	'latitude': 37.8007,
+    'longitude': -122.4447
+}
+locations['Mission'] = {
+	'latitude': 37.7576,
+    'longitude': -122.4105
+}
+locations['Palo_Alto'] = {
+	'latitude': 37.4061,
+    'longitude': -122.1142
+}
+#locations['SFO'] = {
+#	'latitude': 37.6213,
+#	'longitude': -122.3811
+#}
+#locations['SJC'] = {
+#	'latitude': 37.3664,
+#	'longitude': -121.9257
+#}
+#Template for adding new locations:
+#locations[''] = {
+#	'latitude': 
+#	'longitude':
+#}
+
+#Define the set of path's to collect surge data on
+path_connector = '->' #String that will connect locations in our dictionary, eg. "Mission->Marina"
+#Explicit definition of paths:
+#trips_list = ['DNA'+path_connector+'Marina', 'Marina'+path_connector+'DNA', 'Mission'+path_connector+'Marina', 'Marina'+path_connector+'Mission', 'Mission'+path_connector+'Palo_Alto', 'Marina'+path_connector+'Palo_Alto', 'DNA'+path_connector+'Palo_Alto', 'Palo_Alto'+path_connector+'Mission', 'Palo_Alto'+path_connector+'Marina', 'Palo_Alto'+path_connector+'DNA']
+
+#Connect all of the endpoints specified in the locations list:
+trips_list = []
+for start_location in locations:
+	for end_location in locations:
+		trips_list.append(start_location+path_connector+end_location)
+
+#Set up the dictionary that will hold all the trip and surge multiplier data:
 trips = {}
-trips['DNA_to_Marina'] = {
-	'start_latitude': 37.7589,
-    'start_longitude': -122.4125,
-    'end_latitude': 37.8007,
-    'end_longitude': -122.4447,
-    'surge_data': {}
-    }
-trips['Marina_to_DNA'] = {
-	'end_latitude': 37.7589,
-    'end_longitude': -122.4125,
-    'start_latitude': 37.8007,
-    'start_longitude': -122.4447,
-    'surge_data': {}
-    }
-trips['Mission_to_Palo_Alto'] = {
-	'start_latitude': 37.7576,
-    'start_longitude': -122.4105,
-    'end_latitude': 37.4061,
-    'end_longitude': -122.1142,
-    'surge_data': {}
-    }
-trips['Palo_Alto_to_Mission'] = {
-	'end_latitude': 37.7576,
-    'end_longitude': -122.4105,
-    'start_latitude': 37.4061,
-    'start_longitude': -122.1142,
-    'surge_data': {}
-    }
-trips['Mission_to_Mission'] = {
-    'end_latitude': 37.7576,
-    'end_longitude': -122.4105,
-    'start_latitude': 37.7576,
-    'start_longitude': -122.4105,
-    'surge_data': {}
-}
-trips['Marina_to_Marina'] = {
-    'end_latitude': 37.7589,
-    'end_longitude': -122.4125,
-    'start_latitude': 37.7589,
-    'start_longitude': -122.4125,
-    'surge_data': {}
-}
-trips['Palo_Alto_to_Palo_Alto'] = {
-    'end_latitude': 37.4061,
-    'end_longitude': -122.1142,
-    'start_latitude': 37.4061,
-    'start_longitude': -122.1142,
-    'surge_data': {}
-}
-trips['DNA_to_DNA'] = {
-    'start_latitude': 37.7589,
-    'start_longitude': -122.4125,
-    'end_latitude': 37.7589,
-    'end_longitude': -122.4125,
-    'surge_data': {}
-}
-
-
-parameters_dna_to_marina = {
-    'server_token': server_token,
-    'start_latitude': 37.7589,
-    'start_longitude': -122.4125,
-    'end_latitude': 37.8007,
-    'end_longitude': -122.4447
-}
-
-parameters_marina_to_dna = {
-    'server_token': server_token,
-    'end_latitude': 37.7589,
-    'end_longitude': -122.4125,
-    'start_latitude': 37.8007,
-    'start_longitude': -122.4447
-}
-
-parameters_mission_to_palo_alto = {
-    'server_token': server_token,
-    'start_latitude': 37.7576,
-    'start_longitude': -122.4105,
-    'end_latitude': 37.4061,
-    'end_longitude': -122.1142
-}
-
-parameters_palo_alto_to_mission = {
-    'server_token': server_token,
-    'end_latitude': 37.7576,
-    'end_longitude': -122.4105,
-    'start_latitude': 37.4061,
-    'start_longitude': -122.1142
-}
-
-parameters_mission_to_mission = {
-    'server_token': server_token,
-    'end_latitude': 37.7576,
-    'end_longitude': -122.4105,
-    'start_latitude': 37.7576,
-    'start_longitude': -122.4105
-}
-
-parameters_marina_to_marina = {
-    'server_token': server_token,
-    'end_latitude': 37.7589,
-    'end_longitude': -122.4125,
-    'start_latitude': 37.7589,
-    'start_longitude': -122.4125
-}
-
-parameters_palo_alto_to_palo_alto = {
-    'server_token': server_token,
-    'end_latitude': 37.4061,
-    'end_longitude': -122.1142,
-    'start_latitude': 37.4061,
-    'start_longitude': -122.1142
-}
-
-parameters_dna_to_dna = {
-    'server_token': server_token,
-    'start_latitude': 37.7589,
-    'start_longitude': -122.4125,
-    'end_latitude': 37.7589,
-    'end_longitude': -122.4125
-}
+for trip in trips_list:
+	trips[trip] = {
+		'start_latitude': locations[trip[0:trip.find(path_connector)]]['latitude'],
+		'start_longitude': locations[trip[0:trip.find(path_connector)]]['longitude'],
+		'end_latitude': locations[trip[trip.find(path_connector)+len(path_connector):len(trip)]]['latitude'],
+		'end_longitude': locations[trip[trip.find(path_connector)+len(path_connector):len(trip)]]['longitude'],
+		'surge_data': {}
+	}
+print trips
 
 #Specify the url to query for the API request
 #url = 'https://api.uber.com/v1/products'
 url = 'https://api.uber.com/v1/estimates/price'
 
 #Set up the output file to store the data
-output_file = open("Surge_data_Jan3_2016-2.txt", "w")
+output_file = open("Surge_data_Jan3_2016.txt", "w")
 output_file.write('{0:50}'.format('Each route has Uber ride types in the order:'))
 for uber_type in uber_types:
 	output_file.write('{0:15}'.format(uber_type))
@@ -174,59 +105,6 @@ while(True):
 	print 'trips:'
 	print trips
 
-	#response = requests.get(url, params=parameters_dna_to_marina)
-	#data = response.json()
-	#multiplier_dna_to_marina = data['prices'][0]['surge_multiplier']
-	#print 'DNA to Marina:', data['prices'][0]['surge_multiplier']
-
-	#response = requests.get(url, params=parameters_marina_to_dna)
-	#data = response.json()
-	#multiplier_marina_to_dna = data['prices'][0]['surge_multiplier']
-
-	#response = requests.get(url, params=parameters_mission_to_palo_alto)
-	#data = response.json()
-	#multiplier_mission_to_palo_alto = data['prices'][0]['surge_multiplier']
-	#print 'Mission to Palo Alto:', data['prices'][0]['surge_multiplier']
-
-	#response = requests.get(url, params=parameters_palo_alto_to_mission)
-	#data = response.json()
-	#multiplier_palo_alto_to_mission = data['prices'][0]['surge_multiplier']
-
-	#response = requests.get(url, params=parameters_mission_to_mission)
-	#data = response.json()
-	#print data['prices'], len(data['prices'])
-	#print data['prices'][0]['localized_display_name']
-	#print data['prices'][1]['localized_display_name']
-	#print data['prices'][2]['localized_display_name']
-	#print data['prices'][3]['localized_display_name']
-	#print data['prices'][4]['localized_display_name']
-	#print data['prices'][5]['localized_display_name']
-	#print data['prices'][6]['localized_display_name']
-	#print data['prices'][7]['localized_display_name']
-	#multiplier_mission_to_mission = data['prices'][0]['surge_multiplier']
-	#print type(data['prices'])
-	#for index, val in enumerate(data['prices']):
-	#	print index, val['localized_display_name']
-	#uberX_index = [index for index, val in enumerate(data['prices']) if val['localized_display_name'] == 'uberX'][0]
-	#uberXL_index = [index for index, val in enumerate(data['prices']) if val['localized_display_name'] == 'uberXL'][0]
-	#uberSELECT_index = [index for index, val in enumerate(data['prices']) if val['localized_display_name'] == 'uberSELECT'][0]
-	#uberBLACK_index = [index for index, val in enumerate(data['prices']) if val['localized_display_name'] == 'UberBLACK'][0]
-	#uberSUV_index = [index for index, val in enumerate(data['prices']) if val['localized_display_name'] == 'UberSUV'][0]
-	#print 'Mission to Mission:', data['prices'][uberX_index]['surge_multiplier']
-	#print 'Mission to Mission:', data['prices'][0]['surge_multiplier'], data['prices'][1]['surge_multiplier'], data['prices'][2]['surge_multiplier'], data['prices'][3]['surge_multiplier'], data['prices'][4]['surge_multiplier']
-
-	#response = requests.get(url, params=parameters_marina_to_marina)
-	#data = response.json()
-	#multiplier_marina_to_marina = data['prices'][0]['surge_multiplier']
-
-	#response = requests.get(url, params=parameters_palo_alto_to_palo_alto)
-	#data = response.json()
-	#multiplier_palo_alto_to_palo_alto = data['prices'][0]['surge_multiplier']
-
-	#response = requests.get(url, params=parameters_dna_to_dna)
-	#data = response.json()
-	#multiplier_dna_to_dna = data['prices'][0]['surge_multiplier']
-
 	#output_file.write('{0:30} {1:29.1f} {2:29.1f} {3:29.1f} {4:29.1f} {5:29.1f} {6:29.1f} {7:29.1f} {8:29.1f}'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()), multiplier_dna_to_marina, multiplier_marina_to_dna, multiplier_mission_to_palo_alto, multiplier_palo_alto_to_mission, multiplier_mission_to_mission, multiplier_marina_to_marina, multiplier_palo_alto_to_palo_alto, multiplier_dna_to_dna))
 	output_file.write('{0:30}'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())))
 	for trip in trips.keys():
@@ -234,4 +112,4 @@ while(True):
 			output_file.write('{0:5.1f}'.format(trips[trip]['surge_data'][uber_type]))
 		output_file.write('     ')
 	output_file.write('\n')
-	time.sleep(30)
+	time.sleep(60)
